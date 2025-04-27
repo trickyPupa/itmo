1cb5882a293a334969a6d1476471857ee21c150a
Alex <144828762+naku0@users.noreply.github.com>
2025-04-27 16:57:55 +0300

    взорвать

--- opi/lab2/svn.sh
+++ opi/lab2/svn.sh
@@ -1,180 +1,176 @@
 #!/bin/bash
 
-# Создание локального репозитория
-svnadmin create repo
-REPO_URL="file://$(pwd)/repo"
+SVN_REPO="$(realpath ./svn_repo)"
+WORKING_DIR="$(realpath ./svn_working)"
+COMMITS_DIR="$(realpath ./commits)"
 
+# Очистка
+rm -rf "$SVN_REPO" "$WORKING_DIR"
+mkdir -p "$SVN_REPO" "$WORKING_DIR"
 
-# Создаём структуру проекта
-cd repo
-svn mkdir -m "project structure" $REPO_URL/trunk $REPO_URL/branches
-cd ..
+# Инициализация репозитория
+svnadmin create "$SVN_REPO"
+svn mkdir "file://$SVN_REPO/trunk" "file://$SVN_REPO/branches" "file://$SVN_REPO/tags" -m "лооол"
 
-# Создание рабочей копии
-svn checkout $REPO_URL/trunk/ wc
-cd wc
+# 0 -- init коммит (красный юзер)
+svn checkout "file://$SVN_REPO/trunk" "$WORKING_DIR/trunk"
+cd "$WORKING_DIR/trunk"
+cp -r "$COMMITS_DIR/0"/* .
+svn add --force .
+svn commit --username red_user -m "коммит0"
 
+# 1 -- бренч1 от 0 (синий юзер)
+svn copy "file://$SVN_REPO/trunk" "file://$SVN_REPO/branches/branch1" -m "1 ветка"
+svn checkout "file://$SVN_REPO/branches/branch1" "$WORKING_DIR/branch1"
+cd "$WORKING_DIR/branch1"
+cp -r "$COMMITS_DIR/1"/* .
+svn add --force .
+svn commit --username blue_user -m "коммит1"
 
-# Ревизия r0 (пользователь 1) {
-unzip -o commits/commit0.zip -d src
-svn add *
-svn commit -m "Initial commit (r0)" --username=red
-echo "- Коммит 0 (red)"
-# }
+# 2 -- бренч2 от 1 (синий юзер)
+svn copy "file://$SVN_REPO/branches/branch1" "file://$SVN_REPO/branches/branch2" -m "2 ветка"
+svn checkout "file://$SVN_REPO/branches/branch2" "$WORKING_DIR/branch2"
+cd "$WORKING_DIR/branch2"
+cp -r "$COMMITS_DIR/2"/* .
+svn add --force .
+svn commit --username blue_user -m "коммит2"
 
+# 3 -- коммит в транке после 0 (красный юзер)
+cd "$WORKING_DIR/trunk"
 svn update
+cp -r "$COMMITS_DIR/3"/* .
+svn add --force .
+svn commit --username red_user -m "коммит3"
 
-# r1 (пользователь 2) {
-svn copy $REPO_URL/trunk $REPO_URL/branches/branch2 -m "Creating branch2"
-svn switch $REPO_URL/branches/branch2
+# 4 -- коммит в транке после 3 (красный юзер)
+cp -r "$COMMITS_DIR/4"/* .
+svn add --force .
+svn commit --username red_user -m "коммит4 Взорвите Мурино"
 
-svn rm * --force
+# 5 -- коммит в транке после 4 (красный юзер)
+cp -r "$COMMITS_DIR/5"/* .
+svn add --force .
+svn commit --username red_user -m "Коммит 5"
 
-unzip -o commits/commit1.zip -d src
-svn add *
-svn commit -m "r1" --username=blue
-echo "- Коммит 1 (blue)"
-# }
-
+# 6 -- коммит в бренче2 после 2 (синий юзер)
+cd "$WORKING_DIR/branch2"
 svn update
+cp -r "$COMMITS_DIR/6"/* .
+svn add --force .
+svn commit --username blue_user -m "коммит 6"
 
-# r2 пользователь 2 {
-svn copy $REPO_URL/branches/branch2 $REPO_URL/branches/branch3 -m "Creating branch3"
-svn switch $REPO_URL/branches/branch3
-
-svn rm * --force
-
-unzip -o commits/commit2.zip -d src
-svn add *
-svn commit -m "r2" --username=blue
-echo "- Коммит 2 (blue)"
-# }
-
-
-# r3-r5 (пользователь 1) {
-svn switch $REPO_URL/trunk
-
-svn rm *
-unzip -o commits/commit3.zip -d src
-svn add *
-svn commit -m "r3" --username=red
-echo "- Коммит 3 (red)"
-
-svn rm *
-unzip -o commits/commit4.zip -d src
-svn add *
-svn commit -m "r4" --username=red
-echo "- Коммит 4 (red)"
-
-svn rm *
-unzip -o commits/commit5.zip -d src
-svn add *
-svn commit -m "r5" --username=red
-echo "- Коммит 5 (red)"
-# }
-
-
-# r6 (пользователь 2) {
-svn switch $REPO_URL/branches/branch3
-
-svn rm *
-unzip -o commits/commit6.zip -d src
-svn add *
-svn commit -m "r6" --username=blue
-echo "- Коммит 6 (blue)"
-# }
-
-# Мердж ревизии r6 с r5 # {
-svn switch $REPO_URL/trunk
-svn merge $REPO_URL/branches/branch3
-
-nano Lab4.java
-svn resolved Lab4.java
-
-## TODO
-##
-## ИСПРАВЛЕНИЕ КОНФЛИКТА
-##
-
-svn add *
-echo "- Слияние r6 и r5"
-
-# Отменяем merge в случае отмены
-# git merge --abort
-
-
-# Ревизии r7-r10 (пользователь 1) {
-svn rm * --force
-unzip -o commits/commit7.zip -d src
-svn add *
-svn commit -m "r7" --username=red
-echo "- Коммит 7 (red)"
-
-svn rm *
-unzip -o commits/commit8.zip -d src
-svn add *
-svn commit -m "r8" --username=red
-echo "- Коммит 8 (red)"
-
-svn rm *
-unzip -o commits/commit9.zip -d src
-svn add *
-svn commit -m "r9" --username=red
-echo "- Коммит 9 (red)"
-
-svn rm *
-unzip -o commits/commit10.zip -d src
-svn add *
-svn commit -m "r10" --username=red
-echo "- Коммит 10 (red)"
-# }
-
-
-# Ревизия r11-r13 (пользователь 2) {
-svn switch $REPO_URL/branches/branch2
-
-svn rm *
-unzip -o commits/commit11.zip -d src
-svn add *
-svn commit -m "r11" --username=blue
-echo "- Коммит 11 (blue)"
-
-svn rm *
-unzip -o commits/commit12.zip -d src
-svn add *
-svn commit -m "r12" --username=blue
-echo "- Коммит 12 (blue)"
-
-svn rm *
-unzip -o commits/commit13.zip -d src
-svn add *
-svn commit -m "r13" --username=blue
-echo "- Коммит 13 (blue)"
-# }
-
-# Мердж ревизии r13 с r10 # {
-svn switch $REPO_URL/trunk
-
-svn merge $REPO_URL/branches/branch2
-
-nano Lab4.java
-svn resolved Lab4.java
-
-## TODO
-## ИСПРАВЛЕНИЕ КОНФЛИКТА ВРУЧНУЮ
-##
-
-svn add *
-echo "- Слияние r13 и r10"
-
-# Отменяем merge в случае отмены
-# git merge --abort
-
-svn rm *
-unzip -o commits/commit14.zip -d src
-svn add *
-svn commit -m "r14" --username=red
-echo "- Коммит 14 (red)"
-# }
-
+# Мердж 6 в trunk (создаем коммит 7)
+cd "$WORKING_DIR/trunk"
 svn update
+svn merge --accept postpone "file://$SVN_REPO/branches/branch2"
+cp -r "$COMMITS_DIR/7"/* .
+svn add --force .
+svn commit --username red_user -m "коммит7 кикфлип подушкой"
+
+svn resolve --accept working F.java
+svn resolve --accept working K.java
+
+cp -r "$COMMITS_DIR/7"/* .
+svn add --force .
+svn commit --username red_user -m "коммит7 кикфлип подушкой (разрешены конфликты)"
+
+# 8 -- коммит в транке после 7 (красный юзер)
+cp -r "$COMMITS_DIR/8"/* .
+svn add --force .
+svn commit --username red_user -m "коммит8 первая додо пицца открылась в сыктывкаре"
+
+# 9 -- коммит в транке после 8 (красный юзер)
+cp -r "$COMMITS_DIR/9"/* .
+svn add --force .
+svn commit --username red_user -m "коммит9 вчера видел как в метро остановили таджика, было забавно"
+
+# 10 -- коммит в транке после 9 (красный юзер)
+cp -r "$COMMITS_DIR/10"/* .
+svn add --force .
+svn commit --username red_user -m "коммит10 сегодня я поел сырников"
+
+# 11 -- коммит в бренч1 после 1 (синий юзер)
+cd "$WORKING_DIR/branch1"
+svn update
+cp -r "$COMMITS_DIR/11"/* .
+svn add --force .
+svn commit --username blue_user -m "коммит11 возвращение блудного сына"
+
+# 12 -- коммит в бренч1 после 11 (синий юзер)
+cp -r "$COMMITS_DIR/12"/* .
+svn add --force .
+svn commit --username blue_user -m "коммит12 я устал это делать"
+
+# 13 -- коммит в бренч1 после 12 (синий юзер)
+cp -r "$COMMITS_DIR/13"/* .
+svn add --force .
+svn commit --username blue_user -m "коммит13 финал?"
+
+# Мердж 13 в trunk (коммит 14)
+cd "$WORKING_DIR/trunk"
+svn update
+svn merge --accept postpone "file://$SVN_REPO/branches/branch1"
+cp -r "$COMMITS_DIR/14"/* .
+svn add --force .
+svn commit --username red_user -m "коммит14 твоя следующая фраза -- мердж конфликт"
+
+
+svn delete F.java.merge-left.r4
+svn delete F.java.working
+svn delete K.java.merge-left.r4
+svn cleanup
+
+cp -r "$COMMITS_DIR/14"/* .
+svn add --force .
+svn resolve --accept working F.java
+svn resolve --accept working K.java
+svn resolve --accept working jYzbZGr65h.ldT
+svn commit --username red_user -m "коммит14 (minor changes)"
+
+
+echo ...................................................................................
+echo ......................................:-==:........................................
+echo ................................:**************##:.................................
+echo ..............................#****############*###%@*.............................
+echo ............................=#*##########%%@%%%%%%##%%%=...........................
+echo ...........................*#######%##########%%%%%@%#%%=..........................
+echo ..........................*%##%%##%%@%%##**#%%%%%%%#%%%%%#:........................
+echo .........................*%%%%%#*==---==========++==+*##%%#........................
+echo .........................#%%@#+==-----------------=====*#%#+.......................
+echo ........................+%%*====------------------======*#%#.......................
+echo ........................*#*+====--------------------=====*#*:......................
+echo ........................**+====----------------------====***:......................
+echo ........................**+===-----------------------====+*#.......................
+echo ........................*+=====----------------------=====**.......................
+echo ........................*====*+*****++=---====++++++++====+*.......................
+echo ........................=+==+=======++======+++===+++++===*+.......................
+echo .........................=====++*#*++++==-====+#%%*++=====*........................
+echo .......................+=================-===-========-===++#......................
+echo ......................:*++==---------=========---------===++*......................
+echo .......................+++==---------=========---------===+=.......................
+echo .......................:+===---------=+==--===---------===+=.......................
+echo ........................+====-------=++++++*+==-------=====:.......................
+echo ........................======------=====--====-------=====........................
+echo ..........................:===-----------=------------===..........................
+echo ...........................====-----===++++++===------==-..........................
+echo ...........................:====----=++*+++++++=----====...........................
+echo ............................:====--==-===++====-==-====............................
+echo ..............................===========--===========.............................
+echo ..............................======------------===+==:............................
+echo .............................:====++============++====--*..........................
+echo ...........................:#-=======++++==++++=======:-#%.........................
+echo .........................:#%#-::====================-:::-%%#.......................
+echo .......................+##%%%-::::=======-=========.::::-%%%%%=....................
+echo ..................-#%%#%%%%%%::::..:===========---..::::-%%@%%%%%#*................
+echo ..............:#%%%%%%%%%%%%%-::::...:=========-:-.:::::=%%@@@@%@%%%%#*............
+echo ...........*#%%%%%%%@%%%%%%%%+::::.:::::..-===::::--::::*%%%%@@@@@%%%%%%%#:........
+echo .......+#%%%%%%%%%%%%@%%%%%%%#:::::::::::::+::::..::::::%%%%%%@@%%%%%%%%%%%%#=.....
+echo ..:+#%%%%%%%%%%%%@@%%%%%%@@%%%::-..........:.:.......:::%%%@@@@@%@@@%%%%%%%%%%%%#*:
+echo "##%%%%%%%%%%%%%%%@@@@@@@@@@@%%:-:....................::+%%@@%@@*#@@@@@@@@@%%%%%%%%%"
+echo "##%%%%%%%%%@%@@@@@@@@@@@@@@@@%*..:...................::%@@@@%@##*@@@@@@@@@@@@@@%%%%"
+echo "#%%%%%%%%%%%%@@@@@@@@@@@@@@@@%%:.:..................:.:%@@@@@@@@@@@@@@@@@@@@@@@@%%%"
+echo %%%%%%%%%%@@@@@@@@@@@@@@@@@@@%%-....................::*%@@@@@@@@@@@@@@@@@@@@@@@@@@%
+echo %%%%%%%%%@@@@@@@@@@@@@@@@@@@@@%#....................:-%@%@@@@@@@@@@@@@@@@@@@@@@@@@%
+echo %%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@%:..........:.........=%%%@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo %%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@%+....................%%@@@@@@@@@@@@@@@@@@@@@@@@@@%@
+echo %%%%%%%@@@@@@@@@@@@@@@@@@@@@@@@@%...................-%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
